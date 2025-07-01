ec3369f9ba26c17b2d8d9247d5810cdd2e9e98dd
Shayne Rimer <shaynerimer@gmail.com>
2025-07-01 07:08:22 -0400

    Added GraphQL study

---
+++ study_resources/graphQL/src/index.js
@@ -0,0 +1,41 @@
+const { PrismaClient } = require('@prisma/client')
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
+
+const prisma = new PrismaClient()
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
+  },
+  Mutation: {
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: {
+    prisma,
+  }
+})
+
+server
+  .listen()
+  .then(({url}) => { console.log(`Server running on ${url}`) })
